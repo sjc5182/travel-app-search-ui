bf6f63abcf5144930ddc40481b9bb8328038db9d
sjc5182 <shiguangchen1987@gmail.com>
2019-02-17 23:08:12 -0800

    First commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DeltaTicket from './components/delta'
+import './styles/styles.less';
+
+
+ReactDOM.render(<DeltaTicket />, document.getElementById('app'));
---
+++ src/components/delta.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DeltaTicket = () => (
+  <div className="outer-border">
+    <div className = "inner-border-top-left"></div>
+    <div className = "inner-border-top-right"></div>
+    <div className = "inner-border-middle-left"></div>
+    <div className = "inner-border-middle-right"></div>
+    <img className = "inner-border-bottom-left" src = "http://www.deltamuseum.org/images/site/history-logo/current_widget.jpg?sfvrsn=71bfda21_2" />
+    <img className = "inner-border-bottom-right" src = "http://www.deltamuseum.org/images/site/history-logo/current_widget.jpg?sfvrsn=71bfda21_2" ></img>
+  </div>
+);
+export default DeltaTicket;
