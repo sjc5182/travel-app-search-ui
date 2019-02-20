50d2b2cf4057cdbb3e107ad5cc4fd370aa038db9
sjc5182 <shiguangchen1987@gmail.com>
2019-02-19 22:49:01 -0800

    tickets ui layout

--- src/components/airlines/delta.js
+++ src/components/airlines/delta.js
@@ -3,15 +3,31 @@ import React from 'react';
 const DeltaTicket = (props) => (
   <div>
     <div className="outer-border">
-      <div className = "inner-content-box">
-        <div className = "inner-border-top-left">
-          <div className = "inner-content">FLIGHT</div>
-          <div className = "inner-content">DESTINATION</div>
-          <div className = "inner-content">PASSENGER</div>
+      <div className = "inner-border-top-left">
+        <div className = "inner-left-content-box">
+          <div className = "inner-left-content">FLIGHT</div>
+          <div className = "inner-left-content">DESTINATION</div>
+          <div className = "inner-left-content">PASSENGER</div>
         </div>
       </div>
-      <div className = "inner-border-top-right"></div>
-      <div className = "inner-border-middle-left"></div>
+      <div className = "inner-border-top-right">
+        <div className = "inner-right-content-box">
+          <div className = "inner-right-content">FLIGHT</div>
+        </div>
+      </div>
+      <div className = "inner-border-middle-left">
+        <div className = "inner-middle-left-content-box">
+          <div className = "inner-middle-left-content">PASSENGER</div>
+          <div className = "inner-middle-left-content">GATE</div>
+          <div className = "inner-middle-left-content">
+            <div className = "tracking-border-top">
+              <div>TRACKING</div>
+              <div>DATA</div>
+              <div>OPTIONS</div>
+            </div>
+          </div>
+        </div>  
+      </div>
       <div className = "inner-border-middle-right"></div>
       <div className = "inner-border-bottom-left"></div>
       <div className = "inner-border-bottom-right"></div>
--- src/components/main.js
+++ src/components/main.js
@@ -11,19 +11,19 @@ class Main extends React.Component {
     }
   }
 
-  componentDidMount(){
-  const config = {
-    headers: {'X-RapidAPI-Key': '9582c9a04bmsh38417e4edecff7dp13f60djsn521655bbeed3'}
-  };
-  axios.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/069a5957-0617-43fd-a7bc-3ab6f3ddcd07", config)
-    .then((result) => {
-      let itineraries = result.data.Itineraries;
-      let legs = result.data.Legs;
-      this.setState({
-        quotes: Quotes.legs(itineraries, legs)
-      })
-    });
-  }
+  // componentDidMount(){
+  // const config = {
+  //   headers: {'X-RapidAPI-Key': '9582c9a04bmsh38417e4edecff7dp13f60djsn521655bbeed3'}
+  // };
+  // axios.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/069a5957-0617-43fd-a7bc-3ab6f3ddcd07", config)
+  //   .then((result) => {
+  //     let itineraries = result.data.Itineraries;
+  //     let legs = result.data.Legs;
+  //     this.setState({
+  //       quotes: Quotes.legs(itineraries, legs)
+  //     })
+  //   });
+  // }
 
   render() {
     return (
