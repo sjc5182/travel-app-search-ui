7c7cf0f82c361f5a4cad997a314cb93d07adf2a6
sjc5182 <shiguangchen1987@gmail.com>
2019-02-21 12:33:00 -0800

    side-bar properties

--- src/components/side-bars/left-bar.js
+++ src/components/side-bars/left-bar.js
@@ -4,6 +4,9 @@ const LeftBar = () => (
   <div className = "left-bar-container">
     <p>Your Info</p>
     <p>Adults</p>
+    <span>OutBound</span>
+    <span>From</span>
+    <span>To</span>
   </div>
 )
 
