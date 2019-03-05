8a4f7c17cbb425a0e028c2d5de8bc666f0d87774
sjc5182 <shiguangchen1987@gmail.com>
2019-03-05 11:53:30 -0800

    company logo

--- src/components/side-bars/logo-bar.js
+++ src/components/side-bars/logo-bar.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const LogoBar = () => (
   <div className = "logo-bar-container">
-    <div className = "logo-plane" />
-    <div className = "logo-lookup" />
+    <div className = "logo-bar-border">
+      <div className = "logo-plane" />
+      <div className = "logo-lookup" />
+    </div>
+    <div className = "logo-button">
+      <button className = "button-currency">USD</button>
+      <button className = "button-log-in">Log in</button>
+    </div>
   </div>
 )
 
