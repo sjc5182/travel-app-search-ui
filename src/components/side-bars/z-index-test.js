c064a4f5a4158ed913327f4d1ae9264e52e7ee8d
sjc5182 <shiguangchen1987@gmail.com>
2019-03-10 23:22:39 -0700

    animation css work

--- src/components/side-bars/z-index-test.js
+++ src/components/side-bars/z-index-test.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 const zIndexTest = () => (
-  <div className = "zcontainer">
-    <div className = "ztest"> 
-      hello this is good
-    </div>
-    <div className = "ztest1"> 
-      test zindex
-    </div>
-    <div className = "ztest2"> 
-      test zindex
+  <div className = "wrapper">
+    <div className = "zcontainer">
+      <a href="#slider-1">1</a>
+      <a href="#slider-2">2</a>
+      <a href="#slider-3">3</a>
+      <a href="#slider-4">4</a>
+      <a href="#slider-5">5</a>
+      <div className = "slides">
+        <div className = "ztest1" id="slider-1">1</div>
+        <div className = "ztest2" id="slider-2">2</div>
+        <div className = "ztest3" id="slider-3">3</div>
+        <div className = "ztest4" id="slider-4">4</div>
+        <div className = "ztest5" id="slider-5">5</div>
+      </div>
     </div>
   </div>
 )
